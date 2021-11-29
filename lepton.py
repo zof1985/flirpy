5df402c0b105903b4334f2ed55260e5100ab6fa3
Luca Zoffoli <zof1985@gmail.com>
2021-11-29 20:47:35 +0100

    bug fix

--- lepton.py
+++ lepton.py
@@ -149,7 +149,7 @@ class LeptonCamera:
 
         # update the list of collected data
         if self.is_recording():
-            self._data[timestamp] = img.astype
+            self._data[timestamp] = img.astype(np.float16)
 
     def get_last(self):
         """
@@ -263,7 +263,7 @@ class LeptonCamera:
             a 3D array where each the first dimension correspond to each sample.
         """
         t = np.array(list(self._data.keys()))
-        x = np.atleast_3d(list(self._data.values())).astype(np.float16)
+        x = np.atleast_3d(list(self._data.values()))
         return t, x
 
     def to_npz(self, filename):
