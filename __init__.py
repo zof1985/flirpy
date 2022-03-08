7e293a3032a82b11ca1337a56aaa59603f63a8ad
Luca Zoffoli <zof1985@gmail.com>
2022-03-08 16:25:27 +0100

    bug fix

--- __init__.py
+++ __init__.py
@@ -7,7 +7,8 @@ import platform
 # check whether python is running as 64bit or 32bit
 # to import the right .NET dll
 folder = "x64" if platform.architecture()[0] == "64bit" else "x86"
-sys.path.append(os.path.sep.join([os.getcwd(), "flirpy", folder]))
+path = os.path.sep.join(__file__.split(os.path.sep)[:-1])
+sys.path.append(os.path.sep.join([path, folder]))
 clr.AddReference("LeptonUVC")
 clr.AddReference("ManagedIR16Filters")
 
